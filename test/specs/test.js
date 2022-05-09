99c52241e78418d780f0e72490dc7273b1a9a2e3
Elena Shepilova <shepilova13@gmail.com>
2022-05-09 20:31:48 +0200

    added 3 test

--- test/specs/test.js
+++ test/specs/test.js
@@ -28,7 +28,15 @@ describe("Test1", () => {
 
     await expect(browser).toHaveUrlContaining("user");
     await expect(`button[ng-reflect-fx-flex="50"]`).not.toBeClickable();
+  });
 
-    await browser.pause(5000);
+  it("check redirecting to another page", async () => {
+    await browser.url(`https://webdriver.io/`);
+
+    await $(
+      `img[src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"]`
+    ).click();
+
+    await expect(browser).toHaveUrl("https://reactjs.org/");
   });
 });
